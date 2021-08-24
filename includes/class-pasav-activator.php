ec9f63067b4fc7979c9cb91af1b3ccfea0451ac4
mauriac <azouamauriac@gmail.com>
2021-08-24 20:05:07 +0100

    Initial commit

---
+++ includes/class-pasav-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       #
+ * @since      1.0.0
+ *
+ * @package    Pasav
+ * @subpackage Pasav/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Pasav
+ * @subpackage Pasav/includes
+ * @author     Mauriac Azoua <azouamauriac@gmail.com>
+ */
+class Pasav_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-pasav-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       #
+ * @since      1.0.0
+ *
+ * @package    Pasav
+ * @subpackage Pasav/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pasav
+ * @subpackage Pasav/includes
+ * @author     Mauriac Azoua <azouamauriac@gmail.com>
+ */
+class Pasav_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
